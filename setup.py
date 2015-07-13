a1e513126c4b2f1019032e791764dd98bce47c87
Peter Elmers <peter.elmers@yahoo.com>
2015-07-13 10:53:37 -0700

    Integrate with mozbuild.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Mozilla Foundation',
     license='MPL 2.0',
     packages=find_packages(),
-    install_requires=['ply>=3.6<4.0'],
+    install_requires=['ply>=3.6,<4.0'],
     url='https://github.com/pelmers/xpidl',
     entry_points={'console_scripts': ['header.py = xpidl.header:main']},
     keywords=['xpidl', 'parser']
